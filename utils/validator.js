890420f09d35abf96588aae583b169a1be49fd9a
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-10 00:43:25 +0300

    added get all products route and func and validate filters

--- utils/validator.js
+++ utils/validator.js
@@ -1,11 +1,17 @@
 export function checkCartBody(body) {
-    const parameters = ["customerId", "productId", "quantity"];
+    const ALLOWED_PARAMS = ["customerId", "productId", "quantity"];
     const arrBody = Object.keys(body);
     if (arrBody.length !== 3) {
         return false;
     }
-    if (!parameters.every((param) => arrBody.includes(param))) {
+    if (!ALLOWED_PARAMS.every((param) => arrBody.includes(param))) {
         return false;
     }
     return true;
+}
+
+export function validateQueryKeys(query) {
+    const ALLOWED_KEYS = ["inStock", "maxPrice", "search"];
+    const queryKeys = Object.keys(query);
+    return queryKeys.every((key) => ALLOWED_KEYS.includes(key));
 }
