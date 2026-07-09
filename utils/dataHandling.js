6469437b5e4afa0a56a92cd7b083f778eb24c4c5
elyasaf waisman <elyasafw1997@gmail.com>
2026-07-09 11:50:00 +0300

    added demo customers, and created errors class, and getCustomerCart func

--- utils/dataHandling.js
+++ utils/dataHandling.js
@@ -6,20 +6,12 @@ env.config();
 const BASE_PATH = process.env.DB_BASE_PATH;
 
 export async function readData(fileName) {
-    try {
-        const fullPath = `${BASE_PATH}/${fileName}`;
-        const data = await fs.readFile(fullPath, "utf8");
-        return JSON.parse(data);
-    } catch (error) {
-        return error.message;
-    }
+    const fullPath = `${BASE_PATH}/${fileName}`;
+    const data = await fs.readFile(fullPath, "utf8");
+    return JSON.parse(data);
 }
 
 export async function writeData(fileName, data) {
-    try {
-        const fullPath = `${BASE_PATH}/${fileName}`;
-        await fs.writeFile(fullPath, data, "utf8");
-    } catch (error) {
-        return error.message;
-    }
+    const fullPath = `${BASE_PATH}/${fileName}`;
+    await fs.writeFile(fullPath, data, "utf8");
 }
